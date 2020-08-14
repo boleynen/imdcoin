ee3cba4567be8382c6cb636439c3e0921bb29d9a
boleynen <58037455+boleynen@users.noreply.github.com>
2020-08-14 17:24:33 +0200

    cleaned some code + added explanation on every step

--- index.php
+++ index.php
@@ -421,85 +421,87 @@ if(!empty($_POST['claim-gift-btn'])){
 
     <script>
 
-        var searchInput = document.querySelector("#search-name");
-        var searchResult = document.querySelector("#suggesstion-box");
-        let timer = null;
-
-        searchInput.addEventListener('input', function(event){
-            if (timer) {
-                clearTimeout(timer)
-            }
-            timer = setTimeout(() => {
-                const input = searchInput.value;
-                if(input.length > 2){
-                    
-                    fetch('./ajax/search.php?name='+input)
-                    .then(response => {
-                        return response.json();
-                    })
-                    .then(result => {
-                        var results = result.body;
-                        searchResult.innerHTML='';
-                        addItem(results);
-                    })
-                    .catch((error) => console.log(error))
-                } else if(input.length < 2){
-                    searchResult.innerHTML='';
-                }
-            }, 500)
-        });
-
-        // create new option item
-        function addItem(results) {
-            results.forEach(user => {
-                let selectItem = document.createElement('option');
-                selectItem.setAttribute("value", user['id']);
-                selectItem.setAttribute("data-toggle", "modal");
-                selectItem.setAttribute("data-target", "#exampleModalCenter2");              
-                selectItem.className = 'selected_person list-group-item list-group-item-action pay-persons';  
-                var textnode = document.createTextNode(user['name']);         
-                selectItem.appendChild(textnode);                              
-                searchResult.appendChild(selectItem);
-            })
-        }
-
-        searchResult.addEventListener("click", function (item) {
-            if (item.target && item.target.matches("li.selected_person")) {
-                searchInput.setAttribute('value', item.target.getAttribute('value'));
-            }
-        });
-
-
         function selectName(val) {
-            $("#searchReceiver").val(val);
-            $("#suggesstion-box").hide();
-
-            var pay1 = document.querySelector("#pay-1");
-            var pay2 = document.querySelector("#pay-2");
-
-            pay1.setAttribute('style', 'display:none !important');
-            pay2.setAttribute('style', 'display:flex !important');
-
-        }
-
-        var backBtn = document.querySelector("#backBtn");
-
-        backBtn.addEventListener("click", toggleModal);
-
-        function toggleModal() {
-            var pay1 = document.querySelector("#pay-1");
-            var pay2 = document.querySelector("#pay-2");
-            pay1.setAttribute('style', 'display:flex !important');
-            pay2.setAttribute('style', 'display:none !important');
-        };
-
-        $(document).ready(function() {
-            setInterval(refreshCurrency, 5000);
-            function refreshCurrency() {
-                $('#my-currency h3').load('index.php #my-currency h3');
-                $('#my-transactions-container').load('index.php #my-transactions');
+                $("#searchReceiver").val(val);
+                $("#suggesstion-box").hide();
+            
+                var pay1 = document.querySelector("#pay-1");
+                var pay2 = document.querySelector("#pay-2");
+            
+                pay1.setAttribute('style', 'display:none !important');
+                pay2.setAttribute('style', 'display:flex !important');
+            
             }
-        });
+
+            // search for person autocomplete'
+            // search input
+            var searchInput = document.querySelector("#search-name");
+            // search output
+            var searchResult = document.querySelector("#suggesstion-box");
+            // create timer
+            let timer = null;
+
+            // add event listener for input field
+            searchInput.addEventListener('input', function(event){
+                if (timer) {
+                    clearTimeout(timer)
+                }
+                // start timer
+                timer = setTimeout(() => {
+                    // input value from search
+                    const input = searchInput.value;
+                    // length must be 2 characters
+                    if(input.length > 2){
+                        // fetch data from search.php,
+                        // $_GET['name'] (in search.php) = the search input
+                        fetch('./ajax/search.php?name='+input)
+                        .then(response => {
+                            // then return response in json format
+                            return response.json();
+                        })
+                        .then(result => {
+                            // results is data from results->body
+                            var results = result.body;
+                            // reset innerHTML of list items
+                            searchResult.innerHTML='';
+                            // execute addItem function, attach results 
+                            addItem(results);
+                        })
+                        .catch((error) => console.log(error))
+                    } else if(input.length < 2){
+                        searchResult.innerHTML='';
+                    }
+                }, 500)
+            });
+
+        // create new option item function
+            function addItem(results) {
+                // foreach results item, as user
+                results.forEach(user => {
+                    // create new option HTML element
+                    let selectItem = document.createElement('option');
+                    // set value as the selected users ID
+                    selectItem.setAttribute("value", user['id']);
+                    // set data attributes for modals (bootstrap)
+                    selectItem.setAttribute("data-toggle", "modal");
+                    selectItem.setAttribute("data-target", "#exampleModalCenter2"); 
+                    // set classes for new option item             
+                    selectItem.className = 'selected_person list-group-item list-group-item-action pay-persons';  
+                    // create text node
+                    var textnode = document.createTextNode(user['name']);  
+                    // attach text node to new option item       
+                    selectItem.appendChild(textnode);     
+                    // show new option item in output                     
+                    searchResult.appendChild(selectItem);
+                });
+            }
+
+        // when option item is clicked, attach persons ID to input
+            searchResult.addEventListener("click", function (item) {
+                if (item.target && item.target.matches("li.selected_person")) {
+                    searchInput.setAttribute('value', item.target.getAttribute('value'));
+                }
+            });
     </script>
 </body>
 
--- js.js
+++ js.js
@@ -1,120 +1,53 @@
 
-function goback(){
-    window.history.back();
-}
-
-// $('#myModal').on('shown.bs.modal', function () {
-//     $('#myInput').trigger('focus')
-//   });
-
-var elements = document.getElementsByClassName("pay-persons");
-
-var myFunction = function() {
-    var pay1 = document.querySelector("#pay-1");
-    var pay2 = document.querySelector("#pay-2");
-    pay2.setAttribute('style', 'display:flex !important');
-    pay1.setAttribute('style', 'display:none !important');
-};
-
-
-
-
-for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('click', myFunction, false);
-}
-
-var giftAlert = document.querySelector("#gift-alert");
-var giftAlertBtn = document.querySelector("#gift-alert-btn");
-
-function createEventListener() {
-    if (giftAlert) {
-        // giftAlertBtn.addEventListener('click', function() {
-            giftAlert.setAttribute('style', 'display:none !important');
-        // });
-        console.log("success")
-    }else{
-        console.log("feil");
+// go to previous page function
+    function goback(){
+        window.history.back();
     }
-}
+
+// every person, list items
+    var elements = document.getElementsByClassName("pay-persons");
 
 
-var searchInput = document.querySelector("#search-name");
-var searchResult = document.querySelector("#suggesstion-box");
-let timer = null;
+// gift alert popup (for first login reward)
+    var giftAlert = document.querySelector("#gift-alert");
+    var giftAlertBtn = document.querySelector("#gift-alert-btn");
 
-searchInput.addEventListener('input', function(event){
-    if (timer) {
-        clearTimeout(timer)
-    }
-    timer = setTimeout(() => {
-        const input = searchInput.value;
-        if(input.length > 2){
-            
-            fetch('./ajax/search.php?name='+input)
-            .then(response => {
-                return response.json();
-            })
-            .then(result => {
-                var results = result.body;
-                searchResult.innerHTML='';
-                addItem(results);
-            })
-            .catch((error) => console.log(error))
-        } else if(input.length < 2){
-            searchResult.innerHTML='';
+// if gist alert exists, create event listener
+    function createEventListener() {
+        if (giftAlert) {
+            // giftAlertBtn.addEventListener('click', function() {
+                giftAlert.setAttribute('style', 'display:none !important');
+            // });
+            console.log("success")
+        }else{
+            console.log("fail");
         }
-    }, 500)
-});
-
-// create new option item
-function addItem(results) {
-    results.forEach(user => {
-        let selectItem = document.createElement('option');
-        selectItem.setAttribute("value", user['id']);
-        selectItem.setAttribute("data-toggle", "modal");
-        selectItem.setAttribute("data-target", "#exampleModalCenter2");              
-        selectItem.className = 'selected_person list-group-item list-group-item-action pay-persons';  
-        var textnode = document.createTextNode(user['name']);         
-        selectItem.appendChild(textnode);                              
-        searchResult.appendChild(selectItem);
-    })
-}
-
-searchResult.addEventListener("click", function (item) {
-    if (item.target && item.target.matches("li.selected_person")) {
-        searchInput.setAttribute('value', item.target.getAttribute('value'));
     }
-});
 
+// toggle modal function
+    // back button on modal
+    var backBtn = document.querySelector("#backBtn");
+    // add event listener to back button
+    backBtn.addEventListener("click", toggleModal);
 
-function selectName(val) {
-    $("#searchReceiver").val(val);
-    $("#suggesstion-box").hide();
+    function toggleModal() {
+        var pay1 = document.querySelector("#pay-1");
+        var pay2 = document.querySelector("#pay-2");
+        // show first modal
+        pay1.setAttribute('style', 'display:flex !important');
+        // hide second modal
+        pay2.setAttribute('style', 'display:none !important');
+    };
 
-    var pay1 = document.querySelector("#pay-1");
-    var pay2 = document.querySelector("#pay-2");
-
-    pay1.setAttribute('style', 'display:none !important');
-    pay2.setAttribute('style', 'display:flex !important');
-
-}
-
-var backBtn = document.querySelector("#backBtn");
-
-backBtn.addEventListener("click", toggleModal);
-
-function toggleModal() {
-    var pay1 = document.querySelector("#pay-1");
-    var pay2 = document.querySelector("#pay-2");
-    pay1.setAttribute('style', 'display:flex !important');
-    pay2.setAttribute('style', 'display:none !important');
-};
-
-$(document).ready(function() {
-    setInterval(refreshCurrency, 5000);
-    function refreshCurrency() {
-        $('#my-currency h3').load('index.php #my-currency h3');
-        $('#my-transactions-container').load('index.php #my-transactions');
-    }
-});
+// show new transactions every 5 seconds
+    $(document).ready(function() {
+        // execute refreshCurrency every 5 seconds
+        setInterval(refreshCurrency, 5000);
+        function refreshCurrency() {
+            // refresh mu currency
+            $('#my-currency h3').load('index.php #my-currency h3');
+            // refresh recent transactions
+            $('#my-transactions-container').load('index.php #my-transactions');
+        }
+    });
 
